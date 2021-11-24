9141f9c720aeed0164d21bee05000d1f42a65be0
Nicolas <nicoeana.azul@gmail.com>
2021-11-23 21:59:06 -0300

    first comit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SignInComponent } from './home/signin/signin.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SignInComponent
+  }
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
